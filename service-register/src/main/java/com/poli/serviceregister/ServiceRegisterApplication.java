a4caa23266b73cf9c24ed4d3aa24bff1dac0c25a
Mateo Gutiérrez Díaz <matheo2203@gmail.com>
2021-09-16 17:34:51 -0500

    Configurando service-register

---
+++ service-register/src/main/java/com/poli/serviceregister/ServiceRegisterApplication.java
@@ -0,0 +1,15 @@
+package com.poli.serviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ServiceRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegisterApplication.class, args);
+    }
+
+}
