111936d54c5c01380b2cb047e38bf6397de2afec
Mateo Gutiérrez Díaz <matheo2203@gmail.com>
2021-09-16 18:04:47 -0500

    resolucion de conflictos merge

--- service-booking/src/main/java/com/poli/servicebooking/ServiceBookingApplication.java
+++ service-booking/src/main/java/com/poli/servicebooking/ServiceBookingApplication.java
@@ -2,10 +2,7 @@ package com.poli.servicebooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class ServiceBookingApplication {
 
