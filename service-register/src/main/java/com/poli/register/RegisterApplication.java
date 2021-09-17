4fd4c6a18d6f4c4577b4ca9206cc60d27243cb2b
Mateo Gutiérrez Díaz <matheo2203@gmail.com>
2021-09-16 22:16:42 -0500

    Eureka server and client

--- service-register/src/main/java/com/poli/register/RegisterApplication.java
+++ service-register/src/main/java/com/poli/register/RegisterApplication.java
@@ -3,6 +3,7 @@ package com.poli.register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableEurekaServer
 @SpringBootApplication
 public class RegisterApplication {
 
